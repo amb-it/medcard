8e8dca38987292f386b02054803da33ee4dd961d
Vladimir Voznyi <vvoz@ciklum.com>
2019-03-28 17:32:57 +0200

    some more stuff

--- frontend-dirty/src/Main.js
+++ frontend-dirty/src/Main.js
@@ -4,10 +4,35 @@ import {
     NavLink,
     BrowserRouter as Router
 } from "react-router-dom";
+// import { createStore } from "redux";
+// import { Provider } from "react-redux";
+// import { reducer } from "./redux/reducer";
 import Home from "./home/Home";
 import Card from "./card/Card";
 
+// let store = createStore(reducer);
+
 export default class Main extends Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {};
+    }
+
+    componentDidMount() {
+        const apiUrl = 'https://reqres.in/api/users';
+
+        return axios.get(apiUrl)
+            .then(response => {
+                this.setState({
+                    users: response.data
+                })
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
     render() {
         return (
             <Router>
---
+++ frontend-dirty/src/redux/reducer.js
@@ -0,0 +1,15 @@
+export default function reducer(state, action) {
+    if (state === undefined) {
+        return {};
+    }
+
+    switch (action.type) {
+        // TODO: here is something to work with Redux
+        // case "increase":
+        //     return { count: count + 1 };
+        // case "decrease":
+        //     return { count: count - 1 };
+        // default:
+        //     return state;
+    }
+}
