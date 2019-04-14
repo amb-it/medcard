275d5b123f76536bbc7c2a264afe9b9d31a7cfa3
Vladimir Voznyi <vvoz@ciklum.com>
2019-04-14 13:54:59 +0300

    add filePond for image uploading

--- frontend/src/card/addCard/Files.js
+++ frontend/src/card/addCard/Files.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 
+import { FilePond, registerPlugin } from 'react-filepond';
+import 'filepond/dist/filepond.min.css';
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css';
+
+registerPlugin(FilePondPluginImagePreview);
+
 export default class Files extends Component {
     render() {
         return (
-            <div>
-                Files will be here
+            <div class="mb-5">
+                <FilePond allowMultiple={true}/>
             </div>
         );
     }
