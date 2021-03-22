4f3f6a4ba7cca002a8cbf0d33bc5475bb908bec5
Vladimir Voznyi <ambitmain@gmail.com>
2021-03-22 20:56:16 +0200

    FE: home page. Increase card boarders

--- frontend/src/home/ShortCard.js
+++ frontend/src/home/ShortCard.js
@@ -52,7 +52,7 @@ export default class ShortCard extends Component {
                         : ''}
                     </div>
                 </NavLink>
-                <hr />
+                {/*<hr />*/}
             </div>
         );
     }
