e5346cd2df6a7f678f3064e75f1cf49951bd8685
Vladimir Voznyi <ambitmain@gmail.com>
2023-09-18 14:20:38 +0300

    FE: profile. edit. i18n

--- frontend/src/profile/ProfileEdit.js
+++ frontend/src/profile/ProfileEdit.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import {NavLink} from "react-router-dom";
 import axios from "axios";
+import {FormattedMessage} from "react-intl";
 
 export default class ProfileEdit extends Component {
     constructor(props, context) {
@@ -61,7 +62,12 @@ export default class ProfileEdit extends Component {
                         <NavLink to="/profile">
                             <span className='oi oi-caret-left'/>
                         </NavLink>
-                        <span className='page_title'>Редактировать профиль</span>
+                        <span className='page_title'>
+                            <FormattedMessage
+                                id="profile.edit.title"
+                                defaultMessage="Edit profile"
+                            />
+                        </span>
 
                         {/*<button*/}
                         {/*    onClick={this.saveProfile}*/}
@@ -80,68 +86,85 @@ export default class ProfileEdit extends Component {
                                 onChange={this.onInputChange}
                                 value={profile.name}
                                 id="name"
-                                type="text" className="form-control" placeholder="ФИО" />
+                                type="text" className="form-control" />
                         </div>
                         <h2 className="badge badge-secondary">
-                            дата рождения
+                            <FormattedMessage
+                                id="profile.birth-date"
+                                defaultMessage="Birth date"
+                            />
                         </h2>
                         <div className="input-group mb-3">
                             <input
                                 onChange={this.onInputChange}
                                 id="birthdate"
                                 defaultValue={profile.birthdate}
-                                type="date" className="form-control" placeholder="дата" />
+                                type="date" className="form-control" />
                         </div>
                         <h2 className="badge badge-secondary">
-                            вес (кг)
+                            <FormattedMessage
+                                id="profile.edit.weight"
+                                defaultMessage="Weight (kg)"
+                            />
                         </h2>
                         <div className="input-group mb-3">
                             <input
                                 onChange={this.onInputChange}
                                 value={profile.weight}
                                 id="weight"
-                                type="number" className="form-control" placeholder="вес (кг)" />
+                                type="number" className="form-control" />
                         </div>
                         <h2 className="badge badge-secondary">
-                            {/*<span className="oi oi-home" title="icon name" aria-hidden="true" />*/}
-                            Группа крови и резус фактор
+                            <FormattedMessage
+                                id="profile.edit.blood-rf"
+                                defaultMessage="Blood group and Rhesus factor"
+                            />
                         </h2>
                         <div className="input-group mb-3">
                             <input
                                 onChange={this.onInputChange}
                                 value={profile.blood}
                                 id="blood"
-                                type="text" className="form-control" placeholder="Группа крови и резус фактор" />
+                                type="text" className="form-control" />
                         </div>
                         <h2 className="badge badge-secondary">
-                            Хронические заболевания
+                            <FormattedMessage
+                                id="profile.chronic-diseases"
+                                defaultMessage="Chronic diseases"
+                            />
                         </h2>
                         <div className="input-group mb-3">
                             <textarea
                                 onChange={this.onInputChange}
                                 value={profile.chronics}
                                 id="chronics"
-                                rows="2" className="form-control" placeholder="Хронические заболевания" />
+                                rows="2" className="form-control" />
                         </div>
                         <h2 className="badge badge-secondary">
-                            Аллергии
+                            <FormattedMessage
+                                id="profile.allergies"
+                                defaultMessage="Allergies"
+                            />
                         </h2>
                         <div className="input-group mb-3">
                             <textarea
                                 onChange={this.onInputChange}
                                 value={profile.allergies}
                                 id="allergies"
-                                rows="2" className="form-control" placeholder="Аллергия" />
+                                rows="2" className="form-control" />
                         </div>
                         <h2 className="badge badge-secondary">
-                            Другое
+                            <FormattedMessage
+                                id="profile.other"
+                                defaultMessage="Other"
+                            />
                         </h2>
                         <div className="input-group mb-3">
                             <textarea
                                 onChange={this.onInputChange}
                                 value={profile.other}
                                 id="other"
-                                rows="4" className="form-control" placeholder="Другое" />
+                                rows="4" className="form-control" />
                         </div>
                     </div>
 
@@ -150,14 +173,21 @@ export default class ProfileEdit extends Component {
                     <div className='row md-5'>
                         <div className='col-6 text-center'>
                             <NavLink to='/profile' className='btn btn-outline-danger menu_button'>
-                                отмена
+                                <FormattedMessage
+                                    id="common.cancel"
+                                    defaultMessage="cancel"
+                                />
                             </NavLink>
                         </div>
                         <div className='col-6 text-center'>
                             <button
                                 onClick={this.saveProfile}
-                                // disabled={!this.state.editCard}
-                                className='btn btn-success'>сохранить</button>
+                                className='btn btn-success'>
+                                <FormattedMessage
+                                    id="common.save"
+                                    defaultMessage="save"
+                                />
+                            </button>
                         </div>
                     </div>
 
